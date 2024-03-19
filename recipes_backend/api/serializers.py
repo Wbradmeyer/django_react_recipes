d8ac42db886bba997e7e266c53737d258a6f4998
W. Brad Meyer <wbradmeyer@gmail.com>
2024-03-18 22:40:23 -0400

    max length name val

--- recipes_backend/api/serializers.py
+++ recipes_backend/api/serializers.py
@@ -8,8 +8,8 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
 
-        if len(data['name']) < 3:
-            raise serializers.ValidationError({'name_error': 'Name must be at least 3 characters.'})
+        if len(data['name']) < 3 or len(data['name']) > 200:
+            raise serializers.ValidationError({'name_error': 'Name must be between 3 and 200 characters.'})
         
         if data['cook_minutes'] < 1 or data['cook_minutes'] > 180:
             raise serializers.ValidationError({'time_error': 'Cook time must be between 1 and 180 minutes.'})
