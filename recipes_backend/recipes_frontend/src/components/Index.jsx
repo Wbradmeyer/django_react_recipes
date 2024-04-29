94b66bba3342799261393cbcf6aa3fc432d8a477
W. Brad Meyer <wbradmeyer@gmail.com>
2024-04-28 21:23:18 -0400

    start login page

--- recipes_backend/recipes_frontend/src/components/Index.jsx
+++ recipes_backend/recipes_frontend/src/components/Index.jsx
@@ -4,7 +4,40 @@ import "./style.css";
 const Index = () => {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <div className="register">
+        <div>
+          <h2>Register</h2>
+          <p className="fields">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" className="input" />
+          </p>
+          <p className="fields">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="input"
+            />
+          </p>
+        </div>
+        <div>
+          <h2>Login</h2>
+          <p className="fields">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" className="input" />
+          </p>
+          <p className="fields">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="input"
+            />
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
