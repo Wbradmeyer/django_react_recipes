5987369ac6a67f544b68f57bfb84137430774efb
W. Brad Meyer <wbradmeyer@gmail.com>
2024-05-06 16:40:27 -0400

    login working in back

--- recipes_backend/api/views.py
+++ recipes_backend/api/views.py
@@ -8,6 +8,7 @@ from rest_framework.authtoken.models import Token
 from recipes.models import Recipe
 from .serializers import RecipeSerializer, UserSerializer
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 class RecipeListView(generics.ListAPIView):
     queryset = Recipe.objects.all()
@@ -49,7 +50,12 @@ def delete_recipe(request, id):
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({'detail':'Not found'}, status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({'token': token.key, 'user': serializer.data})
 
 @api_view(['POST'])
 def register(request):
