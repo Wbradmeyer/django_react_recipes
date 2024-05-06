3db2b25072b8e92b28718b3c3a27514ef4d210e4
W. Brad Meyer <wbradmeyer@gmail.com>
2024-05-06 14:31:16 -0400

    add authtoken

--- recipes_backend/recipes_backend/settings.py
+++ recipes_backend/recipes_backend/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
-
     'recipes',
     'recipes_frontend',
 ]
